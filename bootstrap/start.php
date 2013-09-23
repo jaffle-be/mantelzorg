ec5c614baf09e796f3dbe9d6da1ff8e0aa9e67c4
Thomas Warlop <thomas.warlop@gmail.com>
2013-09-23 22:48:00 +0200

    removed wildcard for environment

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -28,7 +28,7 @@ $app->redirectIfTrailingSlash();
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('local.mantelzorg', 'localhost', '*'),
+	'local' => array('local.mantelzorg'),
 
 ));
 
