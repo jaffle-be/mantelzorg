abfe71a6c8585cf1b169a0d1ee63408d677c27b5
Thomas Warlop <thomas.warlop@gmail.com>
2013-09-24 22:56:34 +0200

    Temporarily removed login action as this is not ready yet

--- app/views/layout/front/footer.blade.php
+++ app/views/layout/front/footer.blade.php
@@ -17,18 +17,18 @@
 <!--            <li><a href="">--><?//= Lang::get('master.footer.help') ?><!--</a></li>-->
         </ul>
 
-        <ul>
-            <li><a id="log-in" href=""><?= Lang::get('master.footer.login') ?></a></li>
-            <li>
-                <div class="login-form">
-                    <form class="form-inline" action="">
-                        <input class="form-control" type="text" name="email" id="" placeholder="<?= Lang::get('master.footer.email') ?>"/>
-                        <input class="form-control" type="password" name="password" placeholder="<?= Lang::get('master.footer.password') ?>"/>
-                        <input class="btn btn-info" type="submit" value="<?= Lang::get('master.footer.inloggen') ?>"/>
-                    </form>
-                </div>
-            </li>
-        </ul>
+<!--        <ul>-->
+<!--            <li><a id="log-in" href="">--><?//= Lang::get('master.footer.login') ?><!--</a></li>-->
+<!--            <li>-->
+<!--                <div class="login-form">-->
+<!--                    <form class="form-inline" action="">-->
+<!--                        <input class="form-control" type="text" name="email" id="" placeholder="--><?//= Lang::get('master.footer.email') ?><!--"/>-->
+<!--                        <input class="form-control" type="password" name="password" placeholder="--><?//= Lang::get('master.footer.password') ?><!--"/>-->
+<!--                        <input class="btn btn-info" type="submit" value="--><?//= Lang::get('master.footer.inloggen') ?><!--"/>-->
+<!--                    </form>-->
+<!--                </div>-->
+<!--            </li>-->
+<!--        </ul>-->
 
     </div>
 
