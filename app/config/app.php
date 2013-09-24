eee34f18d1b8692b2c4adb69e80bc4e9562a403e
Thomas Warlop <thomas.warlop@gmail.com>
2013-09-24 22:22:48 +0200

    set app key

--- app/config/app.php
+++ app/config/app.php
@@ -65,7 +65,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => 'AxZZVjF8GTQO2RSvsp0wCXV6JPOoMKbO',
 
 	/*
 	|--------------------------------------------------------------------------
