7348c265be412697d29c43a86504335c9c13e8d1
Thomas Warlop <thomas.warlop@gmail.com>
2013-09-25 18:21:38 +0200

    added copyright statement

--- app/views/layout/front/footer.blade.php
+++ app/views/layout/front/footer.blade.php
@@ -33,6 +33,17 @@
     </div>
 
     <div class="pull-right">
+        <span class="copyright">
+        <?
+        $date = new DateTime();
+        if($date->format('Y') === '2013'){
+            echo 'Copyright ® 2013';
+        }
+        else{
+            echo 'Copyright ® 2013 - ' . $date->format('Y');
+        }
+        ?>
+        </span>
         <a target="_blank" href="http://www.hogent.be">
             <img class="logo-hogent" src="/img/logo-footer.png" alt=""/>
         </a>
