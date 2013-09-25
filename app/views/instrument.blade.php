0d8d90274c439fb7b964cc108fcb06a4d61ee894
Thomas Warlop <thomas.warlop@gmail.com>
2013-09-25 20:27:58 +0200

    added tekst implementation

--- app/views/instrument.blade.php
+++ app/views/instrument.blade.php
@@ -6,6 +6,34 @@
 
 @section('content')
 
+<div class="container">
 
+    <div>
+        <h3><?= Lang::get('instrument.s1-title') ?></h3>
+
+        <p><?= Lang::get('instrument.s1-p1') ?></p>
+        <p><?= Lang::get('instrument.s1-p2') ?></p>
+        <p><?= Lang::get('instrument.s1-p3') ?></p>
+    </div>
+
+    <div>
+        <h3><?= Lang::get('instrument.s2-title') ?></h3>
+
+        <p><?= Lang::get('instrument.s2-p1') ?></p>
+        <p><?= Lang::get('instrument.s2-p2') ?></p>
+        <p><?= Lang::get('instrument.s2-p3') ?></p>
+
+    </div>
+
+    <div>
+        <h3><?= Lang::get('instrument.s3-title') ?></h3>
+
+        <p><?= Lang::get('instrument.s3-p1') ?></p>
+        <p><?= Lang::get('instrument.s3-p2') ?></p>
+        <p><?= Lang::get('instrument.s3-p3') ?></p>
+
+    </div>
+
+</div>
 
 @stop
