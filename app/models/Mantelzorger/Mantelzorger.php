9bfc18d19d0bece6e7f0804eb7be56f3cb4c942d
Thomas Warlop <thomas.warlop@gmail.com>
2013-11-28 12:13:06 +0100

    setup base for mantelzorger

---
+++ app/models/Mantelzorger/Mantelzorger.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Mantelzorger;
+
+use Validator;
+use Input;
+use Eloquent;
+use DateTime;
+
+class Mantelzorger extends Eloquent{
+
+    protected $table = 'mantelzorgers';
+
+    protected static $rules = array(
+        'email' => 'email|unique:mantelzorgers,email|required',
+        'firstname' => 'required',
+        'lastname' => 'required',
+        'male' => 'required',
+        'street' => 'required',
+        'postal' => 'required',
+        'city' => 'required',
+        'birthday' => 'required|date_format:d/m/Y'
+    );
+
+    protected $fillable = array(
+        'email', 'firstname', 'lastname', 'male', 'street', 'postal',
+        'city', 'birthday', 'phone'
+    );
+
+    public function setBirthdayAttribute($value)
+    {
+        $this->attributes['birthday'] = DateTime::createFromFormat('d/m/Y', $value);
+    }
+
+    public function validator(array $input = array(), array $rules = array())
+    {
+        if(empty($input))
+        {
+            $input = Input::all();
+        }
+        if(empty($rules))
+        {
+            $rules = static::$rules;
+        }
+
+        return Validator::make($input, $rules);
+    }
+
+} 
