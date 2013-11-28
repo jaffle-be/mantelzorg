15c5ded6e887b126d724e0bdce18399abe111758
Thomas Warlop <thomas.warlop@gmail.com>
2013-11-28 17:18:11 +0100

    enhance mantelzorg index

--- app/views/instellingen/mantelzorgers/index.blade.php
+++ app/views/instellingen/mantelzorgers/index.blade.php
@@ -8,20 +8,31 @@
 
 @foreach($hulpverlener->mantelzorgers as $mantelzorger)
 <div class="row mantelzorger">
-    <div class="col-md-6">
+    <div class="col-md-3">
+        <div class="header">
+            <span><i class="glyphicons user"></i>{{ Lang::get('users.mantelzorger') }}</span>
+        </div>
         <div>
-            <div class="pull-left">{{ $mantelzorger->firstname . ' ' . $mantelzorger->lastname }}</div>
-            <span class="pull-right">{{ $mantelzorger->city }}</span>
-            <div class="clearfix middle"></div>
-            <div class="pull-left">{{ $mantelzorger->email }}</div>
-            <span>{{ $mantelzorger->created_at->format('d/m/Y') }}</span>
+            {{ $mantelzorger->firstname . ' ' . $mantelzorger->lastname }}<br/>
+            {{ $mantelzorger->city }}<br/>
+            {{ $mantelzorger->email }}<br/>
+            {{ $mantelzorger->created_at->format('d/m/Y') }}<br/>
         </div>
     </div>
 
-    <div class="col-md-6">
-        @foreach($mantelzorger->oudere as $oudere)
+    <div class="col-md-9 ouderen">
+        <div>
+            <div class="header clearfix">
+                <span class="pull-left"><i class="glyphicons parents"></i>{{ Lang::get('users.ouderen') }}</span>
+                <a class="btn btn-default pull-right" href="<?= URL::action('Instelling\OudereController@create', array($mantelzorger->id)) ?>"><i class="glyphicon glyphicon-plus"></i></a>
+            </div>
 
+        @foreach($mantelzorger->oudere as $oudere)
+            <div>
+            {{ $oudere->firstname . ' ' . $oudere->lastname }}
+            </div>
         @endforeach
+        </div>
     </div>
 
 </div>
