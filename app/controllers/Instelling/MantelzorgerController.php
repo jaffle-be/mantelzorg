70c1199bf9df3e0a7bb2bf6c9232a9c71164090e
Thomas Warlop <thomas.warlop@gmail.com>
2013-11-28 12:16:35 +0100

    setup controller functionality

--- app/controllers/Instelling/MantelzorgerController.php
+++ app/controllers/Instelling/MantelzorgerController.php
@@ -23,12 +23,29 @@ class MantelzorgerController extends \AdminController{
 
     public function index()
     {
-        $this->layout->content = View::make('instellingen.mantelzorgers')
+        $mantelzorgers = $this->mantelzorger->all();
+        $this->layout->content = View::make('instellingen.mantelzorgers.index', compact(array('mantelzorgers')))
+            ->nest('subnav', 'layout.admin.subnavs.instellingen', array('page' => $this->page));
+    }
+
+    public function create()
+    {
+        $this->layout->content = View::make('instellingen.mantelzorgers.create')
             ->nest('subnav', 'layout.admin.subnavs.instellingen', array('page' => $this->page));
     }
 
     public function store()
     {
+        $validator = $this->mantelzorger->validator();
+
+        if($validator->fails())
+        {
+            return Redirect::back()->withInput()->withErrors($validator->messages());
+        }
+
+        $mantelzorger = $this->mantelzorger->create(Input::except('_token'));
+
+        return Redirect::route('mantelzorgers.index');
     }
 
 } 
