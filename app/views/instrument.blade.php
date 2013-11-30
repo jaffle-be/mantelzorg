53871a3477520a837259f603525c28149142553d
Thomas Warlop <thomas.warlop@gmail.com>
2013-11-30 14:35:23 +0100

    clean up front lang files

--- app/views/instrument.blade.php
+++ app/views/instrument.blade.php
@@ -3,28 +3,28 @@
 <div class="container-small">
 
     <div>
-        <h3><?= Lang::get('instrument.s1-title') ?></h3>
+        <h3><?= Lang::get('front.instrument.s1-title') ?></h3>
 
-        <p><?= Lang::get('instrument.s1-p1') ?></p>
-        <p><?= Lang::get('instrument.s1-p2') ?></p>
-        <p><?= Lang::get('instrument.s1-p3') ?></p>
+        <p><?= Lang::get('front.instrument.s1-p1') ?></p>
+        <p><?= Lang::get('front.instrument.s1-p2') ?></p>
+        <p><?= Lang::get('front.instrument.s1-p3') ?></p>
     </div>
 
     <div>
-        <h3><?= Lang::get('instrument.s2-title') ?></h3>
+        <h3><?= Lang::get('front.instrument.s2-title') ?></h3>
 
-        <p><?= Lang::get('instrument.s2-p1') ?></p>
-        <p><?= Lang::get('instrument.s2-p2') ?></p>
-        <p><?= Lang::get('instrument.s2-p3') ?></p>
+        <p><?= Lang::get('front.instrument.s2-p1') ?></p>
+        <p><?= Lang::get('front.instrument.s2-p2') ?></p>
+        <p><?= Lang::get('front.instrument.s2-p3') ?></p>
 
     </div>
 
     <div>
-        <h3><?= Lang::get('instrument.s3-title') ?></h3>
+        <h3><?= Lang::get('front.instrument.s3-title') ?></h3>
 
-        <p><?= Lang::get('instrument.s3-p1') ?></p>
-        <p><?= Lang::get('instrument.s3-p2') ?></p>
-        <p><?= Lang::get('instrument.s3-p3') ?></p>
+        <p><?= Lang::get('front.instrument.s3-p1') ?></p>
+        <p><?= Lang::get('front.instrument.s3-p2') ?></p>
+        <p><?= Lang::get('front.instrument.s3-p3') ?></p>
 
     </div>
 
--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -12,7 +12,7 @@
 
     <div class="banner">
         <div class="container">
-            <h3><?= Lang::get('beta.sub-header') ?></h3>
+            <h3><?= Lang::get('front.beta.sub-header') ?></h3>
             <div class="row">
                 <div class="picture col-md-6">
                     <img class="img-responsive" src="/images/sfeerfoto.png" alt="beta-banner"/>
@@ -21,15 +21,15 @@
                     <form id="login-form" action="" method="post" class="form-horizontal">
 
                         <p class='intro login'>
-                            <?= Lang::get('login.intro') ?>
+                            <?= Lang::get('front.login.intro') ?>
                         </p>
 
                         <p class="form-wrapper">
 
-                            <label for="email"><?= Lang::get('login.email') ?></label>
+                            <label for="email"><?= Lang::get('front.login.email') ?></label>
                             <?= Form::text('email', null, array('class' => 'form-control'))?>
 
-                            <label for="password"><?= Lang::get('login.password') ?></label>
+                            <label for="password"><?= Lang::get('front.login.password') ?></label>
                             <input type="password" name="password" id="password" class="form-control"/>
 
                         @if($error)
@@ -37,7 +37,7 @@
                         @endif
 
                             <p class="form-actions text-center">
-                                <input class="btn btn-lg btn-primary" type="submit" value="<?= Lang::get('login.sign-in') ?>"/>
+                                <input class="btn btn-lg btn-primary" type="submit" value="<?= Lang::get('front.login.sign-in') ?>"/>
                             </p>
 
                         </p>
