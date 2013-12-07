be494e09fb00f1a8f1abf0cc639d3f58cb9a5ec2
Thomas Warlop <thomas.warlop@gmail.com>
2013-12-07 16:55:05 +0100

    add items to sidebar config

--- app/config/sidebar.php
+++ app/config/sidebar.php
@@ -2,5 +2,73 @@
 
 return array(
 
+    'items' => array(
+        //dashboard
+        array(
+            'href' =>  URL::action('DashController@getIndex'),
+
+            'class' => 'glyphicon glyphicon-dashboard',
+
+            'text' => Lang::get('master.navs.dashboard'),
+
+        ),
+
+        //gebruikers
+        array(
+            'href' => URL::action('HulpverlenerController@index'),
+
+            'class' => 'glyphicons group',
+
+            'text' => Lang::get('master.navs.gebruikers'),
+
+            'condition' => function()
+            {
+                $user = Auth::user();
+                return $user->admin === '1';
+            }
+        ),
+
+        //instrument
+        array(
+            'href' => URL::action('InstrumentController@getIndex'),
+
+            'class' => 'glyphicon glyphicon-list-alt',
+
+            'text' => Lang::get('master.navs.instrument'),
+        ),
+
+        //rapport
+        array(
+            'href' => Url::action('RapportController@getIndex'),
+
+            'class' => 'glyphicon glyphicon-folder-close',
+
+            'text' => Lang::get('master.navs.rapport'),
+        ),
+
+        //instellingen
+        array(
+            'href' => Url::action('Instelling\PersonController@index'),
+
+            'class' => 'glyphicon glyphicon-wrench',
+
+            'text' => Lang::get('master.navs.instellingen'),
+
+            'sublinks' => array(
+                array(
+                    'text' => Lang::get('master.navs.profiel'),
+
+                    'href' => Url::action('Instelling\PersonController@index'),
+                ),
+
+                array(
+                    'text' => Lang::get('master.navs.mantelzorgers'),
+
+                    'href' => URL::route('instellingen.{hulpverlener}.mantelzorgers.index', array(Auth::user()->id)),
+                )
+            )
+        )
+    )
+
 
 );
