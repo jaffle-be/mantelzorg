2057f8916ccf8d0a50d4c9b126f5643870e1eef1
Thomas Warlop <thomas.warlop@gmail.com>
2013-12-07 17:32:37 +0100

    remove all subnav templates

--- app/views/layout/admin/subnavs/users.blade.php
+++
@@ -1,6 +0,0 @@
-<nav>
-    <ul class="nav nav-pills">
-        <li class="<?= $page == 'hulpverlener' ? 'active' : '' ?>"><a href="<?= URL::action('HulpverlenerController@index') ?>"><?= Lang::get('master.navs.hulpverleners') ?></a></li>
-        <li class="<?= $page == 'inschrijving' ? 'active' : '' ?>"><a href="<?= URL::action('InschrijvingController@index') ?>"><?= Lang::get('master.navs.inschrijvingen') ?></a></li>
-    </ul>
-</nav>
