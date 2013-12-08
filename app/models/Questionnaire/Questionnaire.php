0ff04e21233859eed89210c59d686798a6d9684e
Thomas Warlop <thomas.warlop@gmail.com>
2013-12-08 12:17:44 +0100

    add questionnaire model

---
+++ app/models/Questionnaire/Questionnaire.php
@@ -0,0 +1,25 @@
+<?php
+namespace Questionnaire;
+
+use Eloquent;
+use Validator;
+use Input;
+
+class Questionnaire extends Eloquent{
+
+    protected $table = 'questionnaires';
+
+    protected $fillable = array('title', 'active');
+
+    protected static $rules = array(
+        'title' => 'required|unique:questionnaires,title',
+        'active' => 'in:0,1',
+    );
+
+
+    public function validator()
+    {
+        return Validator::make(Input::all(), static::$rules);
+    }
+
+} 
