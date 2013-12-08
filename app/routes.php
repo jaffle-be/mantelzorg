493008b630b9b173579179b44e8067d2675b7d74
Thomas Warlop <thomas.warlop@gmail.com>
2013-12-08 12:18:18 +0100

    cleanup routes

--- app/routes.php
+++ app/routes.php
@@ -19,24 +19,50 @@ Route::model('hulpverlener', 'User');
 Route::pattern('mantelzorger', '\d+');
 Route::model('mantelzorger', 'Mantelzorger\Mantelzorger');
 
+/**
+ * INSTELLINGEN
+ */
 Route::resource('instellingen', 'Instelling\PersonController', array('only' => array('index', 'update')));
 
 Route::resource('instellingen/{hulpverlener}/mantelzorgers', 'Instelling\MantelzorgerController');
 
 Route::resource('instellingen/{mantelzorger}/oudere', 'Instelling\OudereController');
 
+/**
+ * DASH
+ */
+
 Route::controller('dash', 'DashController');
 
+/**
+ * RAPPORT
+ */
 Route::controller('rapport', 'RapportController');
 
+/**
+ * INSTRUMENT
+ */
 Route::controller('instrument','InstrumentController');
 
+/**
+ * INSCHRIJVINGEN
+ */
 Route::resource('inschrijvingen', 'InschrijvingController', array('only' => array('index', 'edit', 'update')));
 
+/**
+ * ORGANISATIONS
+ */
 Route::resource('organisations/{id}/locations', 'Organisation\\LocationController', array('only' => array('index', 'store')));
 
 Route::resource('organisations', 'OrganisationController', array('only' => array('store')));
 
+/**
+ * HULPVERLENERS
+ */
 Route::resource('hulpverleners', 'HulpverlenerController', array('only' => array('index', 'edit', 'update')));
 
+
+/**
+ * INDEX
+ */
 Route::controller('', 'IndexController');
