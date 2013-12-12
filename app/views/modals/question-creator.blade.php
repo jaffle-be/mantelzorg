3c34bd957831ca11fa8bcb795c822a99120f86c2
Thomas Warlop <thomas.warlop@gmail.com>
2013-12-12 15:32:02 +0100

    layout question creator modal

---
+++ app/views/modals/question-creator.blade.php
@@ -0,0 +1,55 @@
+<div class="modal fade" id="question-creator">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title"><?= Lang::get('questionnaires.new_question') ?></h4>
+            </div>
+            <div class="modal-body">
+
+                <label for="question"><?= Lang::get('questionnaires.question') ?></label>
+                @if($errors->has('question'))
+                <span class="errors">{{ $errors->first('question') }}</span>
+                @endif
+                <div class="input-group">
+                    <?= Form::text('question', null, array('class' => 'form-control')) ?>
+                    <span class="input-group-addon"><i class="glyphicons tag"></i></span>
+                </div>
+
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="" id=""/>
+                        <?= Lang::get('questionnaires.summary_question') ?>
+                    </label>
+                </div>
+
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="" id=""/>
+                        <?= Lang::get('questionnaires.multiple_choise_question') ?>
+                    </label>
+                </div>
+
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="" id=""/>
+                        <?= Lang::get('questionnaires.explainable') ?>
+                    </label>
+                </div>
+
+                <?= Form::close() ?>
+
+                <div class="alert alert-danger hide">
+
+                </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal"><?= Lang::get('master.general.cancel') ?></button>
+                <button type="button" class="btn btn-primary"><?= Lang::get('master.general.confirm') ?></button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
