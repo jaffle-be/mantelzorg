09f2c9a36fb9e2ba4e0838eb7da5b7621bf20bc7
Thomas Warlop <thomas.warlop@gmail.com>
2013-12-12 11:11:16 +0100

    add controller methos for creating and saving panels

--- app/controllers/Questionnaire/PanelController.php
+++ app/controllers/Questionnaire/PanelController.php
@@ -2,8 +2,72 @@
 
 namespace Questionnaire;
 
+use Input;
 use View;
 
 class PanelController extends \AdminController{
+
+    /**
+     * @var Panel
+     */
+    protected $panel;
+
+    public function __construct(Panel $panel)
+    {
+        $this->panel = $panel;
+    }
+
+    public function store($questionnaire)
+    {
+        $input = Input::all();
+
+        $input['questionnaire_id'] = $questionnaire->id;
+
+        //find heighest weight, add 10 to it, if no records -> start with 0;
+        $panels = $questionnaire->panels->sortBy(function($panel)
+        {
+            return $panel->panel_weight;
+        }, 'desc');
+
+        $heighestPanel = $panels->last();
+
+        $input['panel_weight'] = $heighestPanel ? $heighestPanel->panel_weight + 10 : 0;
+
+        $validator = $this->panel->validator($input);
+
+        if($validator->fails())
+        {
+            return json_encode(array(
+                'status' => 'error'
+            ));
+        }
+        else
+        {
+            $this->panel->create($input);
+
+            return json_encode(array(
+                'status' => 'oke'
+            ));
+        }
+
+    }
+
+    public function update($questionnaire, $panel)
+    {
+        $panel = $this->panel->find($panel);
+
+        $validator = $this->panel->validator();
+
+        if($validator->fails())
+        {
+            return $validator->messages();
+        }
+        else
+        {
+            $panel->update(Input::all());
+
+            return json_encode(array('status' => 'oke'));
+        }
+    }
 
 } 
