84ffedb9299a61f93b14d92938d1ef4df40e8887
Thomas Warlop <thomas.warlop@gmail.com>
2013-12-12 11:08:02 +0100

    setup basic for questionnaire index view

--- app/views/questionnaire/index.blade.php
+++ app/views/questionnaire/index.blade.php
@@ -11,23 +11,59 @@
 <div class="questionnaires">
 
     @foreach($questionnaires as $questionnaire)
-    <div class="row">
+    <div class="row" data-questionnaire-id="<?= $questionnaire->id ?>">
         <div class="col-md-4 questionnaire">
             <div class="header"><i class="glyphicons notes_2"></i><?= Lang::get('questionnaires.questionnaire') ?></div>
             <div class="body">
-                <?= $questionnaire->title ?>
-                @if($questionnaire->active === '1')
-                <i class="glyphicons-icon ok_2">&nbsp;</i>
-                @endif
+                <?= Form::text('title', $questionnaire->title, array('class' => 'form-control questionnaire-title')) ?>
             </div>
         </div>
-        <div class="col-md-8 panels">
-            <div class="header"><i class="glyphicons list"></i><?= Lang::get('questionnaires.panels') ?></div>
+        <div class="col-md-1 icons">
+            <div class="header">
+                @if($questionnaire->active === '1')
+                <i class="glyphicon glyphicon-check"></i>
+                @else
+                <i class="glyphicon glyphicon-unchecked"></i>
+                @endif
+            </div>
+            <div class="body">
+                <i class="glyphicon glyphicon-floppy-saved fade"></i>
+            </div>
+
+        </div>
+        <div class="col-md-5 panels">
+            <div class="header">
+                <span>
+                    <i class="glyphicons list"></i><?= Lang::get('questionnaires.panels') ?>
+                </span>
+                <a class="btn btn-default pull-right" data-toggle="panel-creator"><i class="glyphicon glyphicon-plus"></i></a>
+            </div>
             <div class="body">
                 <ul>
                 @foreach($questionnaire->panels as $panel)
-                    <li><a href=""><?= $panel->title ?></a></li>
+                    <li data-questionnaire-panel-id="<?= $panel->id ?>">
+                        <div class="input-group">
+                            <div class="input-group-addon"><i class="glyphicon glyphicon-move"></i></div>
+                            <?= Form::text('title', $panel->title , array(
+                                'class' => 'form-control questionnaire-panel-title',
+                                'data-panel-weight' => $panel->panel_weight
+                            )) ?>
+                            <div class="input-group-addon"><i class="glyphicon glyphicon-tag"></i></div>
+                        </div>
+                    </li>
                 @endforeach
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-2 savers">
+            <div class="header">&nbsp;</div>
+            <div class="body">
+                <ul>
+                <? $teller = 1; ?>
+                @while($teller <= count($questionnaire->panels))
+                    <li><i class="glyphicon glyphicon-floppy-saved fade"></i></li>
+                <? $teller++; ?>
+                @endwhile
                 </ul>
             </div>
         </div>
