3e56544530b51228c2241e5cb6878ac56623681f
Thomas Warlop <thomas.warlop@gmail.com>
2013-12-14 12:01:23 +0100

    improve panel seeding, add color

--- app/database/seeds/QuestionnaireSeeder.php
+++ app/database/seeds/QuestionnaireSeeder.php
@@ -11,7 +11,7 @@ class QuestionnaireSeeder extends \Illuminate\Database\Seeder{
         \Questionnaire\Panel::create(array(
             'questionnaire_id' => 1,
             'title' => 'Zorgverlening',
-            'panel_weight' => 0
+            'panel_weight' => 0,
         ));
 
         \Questionnaire\Question::create(array(
@@ -75,25 +75,29 @@ class QuestionnaireSeeder extends \Illuminate\Database\Seeder{
         \Questionnaire\Panel::create(array(
             'questionnaire_id' => 1,
             'title' => 'Mantelzorger',
-            'panel_weight' => 10
+            'panel_weight' => 10,
+            'color' => 'green',
         ));
 
         \Questionnaire\Panel::create(array(
             'questionnaire_id' => 1,
             'title' => 'Relaties',
-            'panel_weight' => 20
+            'panel_weight' => 20,
+            'color' => 'red'
         ));
 
         \Questionnaire\Panel::create(array(
             'questionnaire_id' => 1,
             'title' => 'Omgeving',
-            'panel_weight' => 30
+            'panel_weight' => 30,
+            'color' => 'orange'
         ));
 
         \Questionnaire\Panel::create(array(
             'questionnaire_id' => 1,
             'title' => 'Overzicht',
-            'panel_weight' => 40
+            'panel_weight' => 40,
+            'color' => 'blue',
         ));
     }
 
