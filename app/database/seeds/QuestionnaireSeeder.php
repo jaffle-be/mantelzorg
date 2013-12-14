4c76b0a6297f76a94341bb42ecc34890b6af7039
Thomas Warlop <thomas.warlop@gmail.com>
2013-12-14 11:12:10 +0100

    expand seeder

--- app/database/seeds/QuestionnaireSeeder.php
+++ app/database/seeds/QuestionnaireSeeder.php
@@ -21,6 +21,48 @@ class QuestionnaireSeeder extends \Illuminate\Database\Seeder{
             'question' => 'Ik zou graag een beeld krijgen van de zorgsituatie en hoe u als mantelzorger zich hierbij voelt. Welke taken neemt u op in de zorg? Waarbij helpt u zoal?'
         ));
 
+        \Questionnaire\Choise::create(array(
+            'question_id' => 1,
+            'title' => 'Boodschappen',
+            'value' => 1,
+            'sort_weight' => 0
+        ));
+
+        \Questionnaire\Choise::create(array(
+            'question_id' => 1,
+            'title' => 'Maaltijden bereiden',
+            'value' => 2,
+            'sort_weight' => 10
+        ));
+
+        \Questionnaire\Choise::create(array(
+            'question_id' => 1,
+            'title' => 'Poetsen',
+            'value' => 3,
+            'sort_weight' => 20
+        ));
+
+        \Questionnaire\Choise::create(array(
+            'question_id' => 1,
+            'title' => 'Kledij wassen / strijken',
+            'value' => 4,
+            'sort_weight' => 30
+        ));
+
+        \Questionnaire\Choise::create(array(
+            'question_id' => 1,
+            'title' => 'Persoonsverzorging',
+            'value' => 5,
+            'sort_weight' => 40
+        ));
+
+        \Questionnaire\Choise::create(array(
+            'question_id' => 1,
+            'title' => 'Transfers',
+            'value' => 6,
+            'sort_weight' => 50
+        ));
+
         \Questionnaire\Question::create(array(
             'questionnaire_id' => 1,
             'questionnaire_panel_id' => 1,
