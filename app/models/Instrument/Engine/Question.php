bde3cc0d6f8210418dbb3b03d4345302b6b6bd62
Thomas Warlop <thomas.warlop@gmail.com>
2013-12-25 14:19:18 +0100

    re-add hiding body feature

--- app/models/Instrument/Engine/Question.php
+++ app/models/Instrument/Engine/Question.php
@@ -43,7 +43,7 @@ class Question {
 
     protected function body(Q $question, $first)
     {
-        $output = sprintf('<div class="body %s">', $first ? '' : '');
+        $output = sprintf('<div class="body %s">', $first ? '' : 'hide');
 
         $output .= '<div class="question">' . $question->question . '</div>';
 
