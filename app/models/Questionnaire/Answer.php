a3b2b2cbe7e20549230c214d46a3f24fa55745a2
Thomas Warlop <thomas.warlop@gmail.com>
2014-01-14 20:55:56 +0100

    update answer model to hold session information

--- app/models/Questionnaire/Answer.php
+++ app/models/Questionnaire/Answer.php
@@ -9,9 +9,10 @@ class Answer extends Eloquent{
 
     protected $table = 'questionnaire_answers';
 
-    protected $fillable = array('question_id', 'explanation');
+    protected $fillable = array('question_id', 'explanation', 'session_id');
 
     protected static $rules = array(
+        'session_id' => 'required|exists:questionnaire_survey_sessions,id',
         'question_id' => 'required|exists:questionnaire_questions,id',
         'explanation'
     );
@@ -21,6 +22,11 @@ class Answer extends Eloquent{
         return Validator::make(Input::all(), static::$rules);
     }
 
+    public function session()
+    {
+        return $this->belongsTo('Questionnaire\Session', 'session_id');
+    }
+
     public function question()
     {
         return $this->belongsTo('Questionnaire\Question', 'question_id');
@@ -28,7 +34,7 @@ class Answer extends Eloquent{
 
     public function choises()
     {
-        return $this->belongsToMany('Questionnaire\Choise', 'questionnaire_answer_choises', 'answer_id', 'choise_id');
+        return $this->belongsToMany('Questionnaire\Choise', 'questionnaire_answer_choises', 'answer_id', 'choise_id')->withTimestamps();
     }
 
 }
