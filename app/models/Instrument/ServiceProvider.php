8a1a82784b07004977f5edbf2f4d9f95209ad98c
Thomas Warlop <thomas.warlop@gmail.com>
2014-01-14 20:56:38 +0100

    update service provider for instrument

--- app/models/Instrument/ServiceProvider.php
+++ app/models/Instrument/ServiceProvider.php
@@ -1,13 +1,19 @@
 <?php
 namespace Instrument;
 
+use Mantelzorger\Mantelzorger;
+use Mantelzorger\Oudere;
+use Questionnaire\Answer;
+use Questionnaire\Question;
+use Questionnaire\Session;
+
 class ServiceProvider extends \Illuminate\Support\ServiceProvider{
 
     public function register()
     {
         $this->app['instrument.memorize'] = $this->app->share(function($app)
         {
-            return new Memorize\Questionnaire($app['session.store']);
+            return new Memorize\Questionnaire($app['session.store'], new Answer(), new Question(), new Mantelzorger(), new Oudere(), new Session());
         });
 
         $this->app['instrument.engine'] = $this->app->share(function($app){
