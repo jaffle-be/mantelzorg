c5167446c244120270833ea0d9c336364d6b4256
Thomas Warlop <thomas.warlop@gmail.com>
2014-01-14 19:55:59 +0100

    add model for questionnaire survey session

---
+++ app/models/Questionnaire/Session.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Questionnaire;
+use Eloquent;
+
+
+class Session extends Eloquent{
+
+    protected $table = "questionnaire_survey_sessions";
+
+    protected $fillable = array('mantelzorger_id', 'oudere_id');
+
+    protected static $rules = array(
+        'mantelzorger_id' => 'required|exists:mantelzorgers,id',
+        'oudere_id' => 'required|exists:ouderen,id'
+    );
+
+    public function mantelzorger()
+    {
+        return $this->belongsTo('Mantelzorger\Mantelzorger');
+    }
+
+    public function oudere()
+    {
+        return $this->belongsTo('Mantelzorger\Oudere');
+    }
+
+} 
