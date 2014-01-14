95b9ee1af467e49462d0962dc00d4c43c6653547
Thomas Warlop <thomas.warlop@gmail.com>
2014-01-14 19:53:46 +0100

    create Instrument session migration + fix answer migration

---
+++ app/database/migrations/2013_12_08_100833_createInstrumentSession.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class createInstrumentSession extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('questionnaire_survey_sessions', function($t)
+        {
+            $t->increments('id');
+            $t->integer('oudere_id')->unsigned();
+            $t->foreign('oudere_id')->references('id')->on('ouderen');
+            $t->integer('mantelzorger_id')->unsigned();
+            $t->foreign('mantelzorger_id')->references('id')->on('mantelzorgers');
+            $t->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('questionnaire_survey_sessions');
+	}
+
+}
--- app/database/migrations/2013_12_08_100834_createAnswers.php
+++ app/database/migrations/2013_12_08_100834_createAnswers.php
@@ -14,9 +14,11 @@ class CreateAnswers extends Migration {
 		Schema::create('questionnaire_answers', function($t)
         {
             $t->increments('id');
+            $t->integer('session_id')->unsigned();
+            $t->foreign('session_id')->references('id')->on('questionnaire_survey_sessions');
             $t->integer('question_id')->unsigned();
             $t->foreign('question_id')->references('id')->on('questionnaire_questions');
-            $t->string('explanation', 500);
+            $t->string('explanation', 500)->nullable();
             $t->timestamps();
         });
 	}
