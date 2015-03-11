39899df14babc69a9ef06343902a371de91ef70f
Thomas Warlop <thomas.warlop@gmail.com>
2015-03-11 22:51:11 +0100

    test inschrijving icon
    Signed-off-by: Thomas Warlop <thomas.warlop@gmail.com>

--- app/views/inschrijving/search.blade.php
+++ app/views/inschrijving/search.blade.php
@@ -3,7 +3,7 @@
         <input class="form-control" placeholder="{{ Lang::get('master.tools.zoeken') }}" type="text" name="query" value="{{Input::get('query')}}"/>
 
         <div class="input-group-btn">
-            <a class="btn btn-default" href="{{ route('inschrijvingen.index') }}"><span class="fa fa-times-circle-o"></span></a>
+            <a class="btn btn-default" href="{{ route('inschrijvingen.index') }}"><i class="fa fa-times-circle-o">&nbsp;</i></a>
         </div>
     </div>
     <input type="submit" class="hidden"/>
