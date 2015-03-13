1f7b7e03397e0045efd82c868f247a5a3ae2d480
Thomas Warlop <thomas.warlop@gmail.com>
2015-03-13 07:48:50 +0100

    fix instrument header colors
    also added border
    
    Signed-off-by: Thomas Warlop <thomas.warlop@gmail.com>

--- app/views/instrument/template/header.blade.php
+++ app/views/instrument/template/header.blade.php
@@ -12,7 +12,7 @@
 
         <ul style="display: none;">
             @foreach($panel->questionnaire->panels as $item)
-                <li class="{{ 'panel-' . $item->color }}">
+                <li class="{{ 'panel-' . $panel->color }}">
                     <a data-target-id="{{ $item->id }}" href="">{{ $item->title }}</a>
                 </li>
             @endforeach
