e46f95169645d7521cde0dde064a109b48f47087
Thomas Warlop <thomas.warlop@gmail.com>
2015-04-26 14:39:08 +0200

    add instrument header to scroll calculator
    Signed-off-by: Thomas Warlop <thomas.warlop@gmail.com>

--- app/frontend/js/plugins/viewport.js
+++ app/frontend/js/plugins/viewport.js
@@ -1,33 +1,32 @@
-(function($, app)
-{
+(function ($, app) {
     'use strict';
 
-    app.fixWidth = function()
-    {
+    app.fixWidth = function () {
+
         $("body.tablet").css({
             'max-height': $(window).height(),
             'height': $(window).height()
         });
 
-        var contentHeight = $(window).height() - $("header").outerHeight() - $("#page-header").outerHeight();
+        var contentHeight = $(window).height() - $("header").outerHeight() - $("#page-header").outerHeight() - $('.instrument-header').outerHeight();
 
         $("body.tablet #content").css({
-            'max-height':  contentHeight,
+            'max-height': contentHeight,
             'min-height': contentHeight,
             height: contentHeight
         });
     };
 
     //make sure the container still fits the window after resize or changing tablet orientation.
-    $(window).on('resize orientationChanged', function()
-    {
+    $(window).on('resize orientationChanged', function () {
         app.fixWidth();
     });
 
     app.fixWidth();
 
-    $(window).ready(function(){
+    $(window).ready(function () {
         app.fixWidth()
     });
+
 
 })(window.jQuery, window.app);
