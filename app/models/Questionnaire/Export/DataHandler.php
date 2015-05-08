86edcad85116b43f82f8559757ea16b430b751d1
Thomas Warlop <thomas.warlop@gmail.com>
2015-05-08 16:32:49 +0200

    cleanup
    Signed-off-by: Thomas Warlop <thomas.warlop@gmail.com>

--- app/models/Questionnaire/Export/DataHandler.php
+++ app/models/Questionnaire/Export/DataHandler.php
@@ -2,17 +2,30 @@
 
 use Illuminate\Database\Eloquent\Collection;
 use Maatwebsite\Excel\Classes\LaravelExcelWorksheet;
+use Questionnaire\Session;
 
-class DataHandler {
+class DataHandler
+{
 
+    /**
+     * @var Repository
+     */
     protected $repository;
 
+    /**
+     * @param Repository $repository
+     */
     public function __construct(Repository $repository)
     {
         $this->repository = $repository;
     }
 
-    public function handle(Collection $sessions, $panels, LaravelExcelWorksheet $sheet)
+    /**
+     * @param Collection            $sessions
+     * @param array                 $panels
+     * @param LaravelExcelWorksheet $sheet
+     */
+    public function handle(Collection $sessions, array $panels, LaravelExcelWorksheet $sheet)
     {
         $sessions->load([
             'user',
@@ -25,20 +38,11 @@ class DataHandler {
         $answers = $this->repository->getAnswers($sessionIds);
         $choises = $this->repository->getChoises($sessionIds);
 
-
-
         foreach ($sessions as $session) {
 
             $sessionAnswers = isset($answers[$session['id']]) ? $answers[$session['id']] : [];
 
-            $sessionData = [];
-
-            //add the session id as first column.
-            $sessionData[] = $session->getAttribute('id');
-
-            $sessionData = $this->getUserData($session, $sessionData);
-            $sessionData = $this->getMantelzorgerData($session, $sessionData);
-            $sessionData = $this->getOudereData($session, $sessionData);
+            $sessionData = $this->getBaseData($session);
 
             $session = $session->toArray();
 
@@ -48,55 +52,54 @@ class DataHandler {
             }
 
             $sheet->appendRow($sessionData);
-
         }
     }
 
+    /**
+     * @param array $data
+     * @param       $questions
+     * @param       $session
+     * @param       $answers
+     * @param       $chosen
+     *
+     * @return array
+     */
     protected function answers(array $data, $questions, $session, $answers, $chosen)
     {
         foreach ($questions as $question) {
             //check if the session answered the question
             if ($answer = $this->wasAnswered($answers, $question['id'])) {
-
-                if($question['explainable'])
-                {
-                    $data[] = $answer->explanation;
-                }
-
-                foreach ($question['choises'] as $choise) {
-                    if ($this->wasChecked($chosen, $choise['id'], $answer->id)) {
-                        $data[] = 1;
-                    } else {
-                        $data[] = 0;
-                    }
-                }
+                $data = $this->handleAnswered($data, $chosen, $question, $answer);
             } else {
-
-                if($question['explainable'])
-                {
-                    $data[] = '';
-                }
-
-                foreach ($question['choises'] as $choise) {
-                    $data[] = 0;
-                }
+                $data = $this->handleUnanswered($data, $question);
             }
         }
 
         return $data;
     }
 
-
+    /**
+     * @param array $answers
+     * @param       $questionid
+     *
+     * @return bool
+     */
     protected function wasAnswered(array $answers, $questionid)
     {
         return isset($answers[$questionid]) ? $answers[$questionid] : false;
     }
 
+    /**
+     * @param $choises
+     * @param $choiseid
+     * @param $answerid
+     *
+     * @return bool
+     */
     protected function wasChecked($choises, $choiseid, $answerid)
     {
         return isset($choises[$answerid]) && in_array($choiseid, $choises[$answerid]);
     }
-
 
     /**
      * @param $session
@@ -140,4 +143,65 @@ class DataHandler {
         return $sessionData;
     }
 
+    /**
+     * @param $session
+     *
+     * @return array
+     */
+    protected function getBaseData(Session $session)
+    {
+        $sessionData = [];
+        //add the session id as first column.
+        $sessionData[] = $session->getAttribute('id');
+
+        $sessionData = $this->getUserData($session, $sessionData);
+        $sessionData = $this->getMantelzorgerData($session, $sessionData);
+        $sessionData = $this->getOudereData($session, $sessionData);
+
+        return $sessionData;
+    }
+
+    /**
+     * @param array $data
+     * @param       $chosen
+     * @param       $question
+     * @param       $answer
+     *
+     * @return array
+     */
+    protected function handleAnswered(array $data, $chosen, $question, $answer)
+    {
+        if ($question['explainable']) {
+            $data[] = $answer->explanation;
+        }
+
+        foreach ($question['choises'] as $choise) {
+            if ($this->wasChecked($chosen, $choise['id'], $answer->id)) {
+                $data[] = 1;
+            } else {
+                $data[] = 0;
+            }
+        }
+
+        return $data;
+    }
+
+    /**
+     * @param array $data
+     * @param       $question
+     *
+     * @return array
+     */
+    protected function handleUnanswered(array $data, $question)
+    {
+        if ($question['explainable']) {
+            $data[] = '';
+        }
+
+        foreach ($question['choises'] as $choise) {
+            $data[] = 0;
+        }
+
+        return $data;
+    }
 }
