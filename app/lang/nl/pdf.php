fc36173a5fa91cbbf9e64472aae7cb28ec857296
Thomas Warlop <thomas.warlop@gmail.com>
2015-05-12 17:20:27 +0200

    finalise pdf
    Signed-off-by: Thomas Warlop <thomas.warlop@gmail.com>

---
+++ app/lang/nl/pdf.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'overzicht' => 'Overzicht',
+    'hulpverlener' => 'Hulpverlener',
+    'mantelzorger' => 'Mantelzorger',
+    'hulpbehoevende' => 'Hulpbehoevende',
+
+    'vragenlijst' => 'Vragenlijst',
+    'created'=> 'Aangemaakt op',
+    'updated' => 'Laatst gewijzigd',
+    'answered' => 'Beantwoord',
+
+    'identifier' => 'Volgnummer',
+    'fullname' => 'Naam',
+    'address' => 'Adres',
+    'birthday' => 'Geboortedatum',
+    'email' => 'Email',
+    'phone' => 'Telefoon',
+    'organisation' => 'Organisatie',
+    'location' => 'Locatie',
+
+    'woonsituatie' => 'Woonsituatie',
+    'bel_profiel' => 'Bel profiel',
+    'mantelzorger_relation' => 'Relatie met mantelzorger',
+    'oorzaak_hulpbehoefte' => 'Oorzaak van hulpbehoefte',
+
+    'diagnose' => 'Diagnose',
+
+];
