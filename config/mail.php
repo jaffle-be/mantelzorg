35fe35361cd9398b497759576601ad647c0e59a0
Thomas Warlop <thomas.warlop@gmail.com>
2015-06-21 22:10:11 +0200

    quick run through config files

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => null, 'name' => null],
+	'from' => ['address' => 'systeem@zichtopmantelzorg.be', 'name' => 'Zicht op mantelzorg'],
 
 	/*
 	|--------------------------------------------------------------------------
--- config/queue.php
+++ config/queue.php
@@ -68,7 +68,7 @@ return [
 
 		'redis' => [
 			'driver' => 'redis',
-			'queue'  => 'default',
+			'queue'  => 'mantelzorg',
 			'expire' => 60,
 		],
 
