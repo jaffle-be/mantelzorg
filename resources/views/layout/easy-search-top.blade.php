eab5a3fcc3bf5035b6ec2c33ab9bef3040a36c75
Thomas Warlop <thomas.warlop@gmail.com>
2015-09-28 16:59:59 +0200

    easy search, make sure of decent width

--- resources/views/layout/easy-search-top.blade.php
+++ resources/views/layout/easy-search-top.blade.php
@@ -1,5 +1,5 @@
 <div class="row easy-search">
-    <div class="col-xs-12 col-lg-4 col-lg-offset-4">
+    <div class="col-xs-12 col-lg-6 col-lg-offset-2">
         @include($view)
     </div>
     <div class="col-xs-12 col-lg-4 text-right">
