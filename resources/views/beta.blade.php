78222b4aa5d2f2d1b5e47f438f81e6b92e6bb132
Thomas Warlop <thomas.warlop@gmail.com>
2016-04-23 14:08:13 +0200

    inline beta form

--- resources/views/beta.blade.php
+++ resources/views/beta.blade.php
@@ -1,11 +1,5 @@
 @extends('layout.front.master')
 
-@section('styles')
-@stop
-
-@section('scripts')
-@stop
-
 @section('page-header')
     <h2>{{ Lang::get('front.beta.sub-header') }}</h2>
 @stop
@@ -22,7 +16,49 @@
                     </div>
                     <div class="col-md-6">
 
-                        @include('form')
+                        <form method="post">
+
+                            <h3><?= Lang::get('front.beta.intro-intrested') ?></h3>
+
+                            <div class="form-group">
+                                <label class="control-label" for="beta-firstname"><?= Lang::get('front.beta.firstname') ?></label>
+                                <?= Form::text('firstname', null, array('class' => 'form-control')) ?>
+                            </div>
+
+                            @error('firstname')
+
+                            <div class="form-group">
+                                <label class="control-label" for="beta-lastname"><?= Lang::get('front.beta.lastname') ?></label>
+                                <?= Form::text('lastname', null, array('class' => 'form-control')) ?>
+                            </div>
+
+                            @error('lastname')
+
+                            <div class="form-group">
+                                <label class="control-label" for="beta-email"><?= Lang::get('front.beta.email') ?></label>
+                                <?= Form::text('email', null, array('class' => 'form-control')) ?>
+                            </div>
+
+                            @error('email')
+
+                            <div class="form-group">
+                                <label class="control-label" for="beta-organisation"><?= Lang::get('front.beta.organisation') ?></label>
+                                <?= Form::text('organisation', null, array('class' => 'form-control')) ?>
+                            </div>
+
+                            @error('organisation')
+
+                            <?php if (Session::has('message')): ?>
+                            <div class="alert alert-success">
+                                {{ Lang::get('front.beta.registration-thanks') }}
+                            </div>
+                            <?php endif; ?>
+
+                            <div class="text-center">
+                                <button class="btn btn-lg btn-primary" type="submit"><?= Lang::get('front.beta.register') ?></button>
+                            </div>
+
+                        </form>
 
                     </div>
                 </div>
